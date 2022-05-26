80d19a048458957134f27f7304fa6f70368bddb1
Danilo Ferreira <danilof.queiroz1@gmail.com>
2022-05-26 10:28:45 -0300

    Correção requisito 5, criação Loading Page, remoção dos console.log

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -14,9 +14,6 @@ const playerReducer = (state = INITIAL_STATE, action) => {
       ...state,
       name: action.payload.name,
       gravatarEmail: action.payload.hashEmail,
-      score: action.payload.score,
-      assertions: action.payload.assertions,
-
     };
   default:
     return state;
